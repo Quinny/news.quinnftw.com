a3125fc1fda728eceab14ce86e35647f6ac83a04
Quinn Perfetto <thequinnftw@gmail.com>
2017-05-17 11:10:25 -0700

    added colors for each news source to help discern between different ones

--- static/app.jsx
+++ static/app.jsx
@@ -1,3 +1,26 @@
+const sourceCardStyles = {
+  "Hacker News": {
+    borderColor: "orange",
+    backgroundColor: "rgba(255,165,0,0.1)"
+  },
+  "the morning paper": {
+    borderColor: "purple",
+    backgroundColor: "rgba(128,0,128,0.1)"
+  },
+  "Troy Hunt's Blog": {
+    borderColor: "green",
+    backgroundColor: "rgba(0,128,0,0.1)"
+  },
+  "QuinnFTW.com": {
+    borderColor: "blue",
+    backgroundColor: "rgba(0,0,255,0.1)"
+  },
+  "MalwareTech": {
+    borderColor: "cyan",
+    backgroundColor: "rgba(0,255,255,0.1)"
+  }
+};
+
 class RssFeed extends React.Component {
   constructor(props) {
     super(props);
@@ -5,16 +28,18 @@ class RssFeed extends React.Component {
 
   render() {
     var postFeed = this.props.posts.map((post, index) => {
+      var extraStyle = post.source in sourceCardStyles ? sourceCardStyles[post.source] : {};
       return (
         <div className="row" key={index}>
           <div className="col-md-8 offset-md-2 col-xs-12">
-            <div className="card">
+            <div className="card" style={extraStyle}>
               <div className="card-block">
                 <h4 className="card-title">
                   <a href={post.link}>{post.title}</a>
                 </h4>
                 <h6 className="card-subtitle mb-2 text-muted">{post.source}</h6>
-                {post.comments && <a href={post.comments} className="card-link">Comments</a>}
+                {post.comments &&
+                  <a href={post.comments} className="card-link">Comments</a>}
               </div>
             </div>
           </div>
