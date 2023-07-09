7d62882dbc3b07722fa59965f39b3a97de39ce3a
Quinn Perfetto <thequinnftw@gmail.com>
2023-07-09 11:23:19 -0700

    check in production server settings

--- main.py
+++ main.py
@@ -84,4 +84,4 @@ async def feed(request):
     return response.json(await get_feed())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=80, workers=2, debug=False, access_log=False)
